94cca60a2ce1f36bd8ab31fe39a393d4c42ecc0f
sunya-ai <raj@sunya.tech>
2025-01-20 13:34:45 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import { ThemeProvider } from '@/components/theme-provider';
 import './globals.css';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://sidekick.sunya.ai/'),
+  title: 'Sunya Sidekick',
+  description: 'Sunya Sidekick',
 };
 
 export const viewport = {
