83f146895ce7cfaec3347b6bfd3e6aed61d24254
sunya-ai <raj@sunya.tech>
2025-01-27 22:26:50 -0600

    Update models.ts

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -19,18 +19,7 @@ export const models: Array<Model> = [
     apiIdentifier: 'gpt-4o',
     description: 'For complex, multi-step tasks',
   },
-  {
-    id: 'deepseek-chat',
-    label: 'SUNYA AI DS',
-    apiIdentifier: 'deepseek-chat',
-    description: 'Latest version of DeepSeek chat model',
-  },
-  {
-    id: 'deepseek-reasoner',
-    label: 'SUNYA AI R1',
-    apiIdentifier: 'deepseek-reasoner',
-    description: 'Specialized model for complex reasoning tasks',
-  },
+
 ] as const;
 
 export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
