37066821001630fff240281e083c0032071a838e
sunya-ai <raj@sunya.tech>
2025-02-17 14:43:38 -0600

    Update markdown.tsx

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -10,11 +10,12 @@ import { CodeBlock } from "./code-block"
 import { cn } from "@/lib/utils"
 
 const components: Partial<Components> = {
-  code: ({ node, inline, className, children, ...props }) => {
+  code: ({ node, className, children, ...props }: any) => {
     const match = /language-(\w+)/.exec(className || "")
-    return !inline && match ? (
-      <CodeBlock language={match[1]} value={String(children).replace(/\n$/, "")} {...props} />
-    ) : (
+    if (match && props.inline !== true) {
+      return <CodeBlock language={match[1]} value={String(children).replace(/\n$/, "")} {...props} />
+    }
+    return (
       <code className={className} {...props}>
         {children}
       </code>
