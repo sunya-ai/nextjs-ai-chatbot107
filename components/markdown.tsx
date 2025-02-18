66b2bd3987f569fb0e7fa91118880e489c56bda6
sunya-ai <raj@sunya.tech>
2025-02-18 11:04:47 -0600

    Update markdown.tsx

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -14,9 +14,9 @@ import { ExternalLink } from 'lucide-react';
 const components: Partial<Components> = {
   // @ts-expect-error
   code: CodeBlock,
-  pre: ({ children }: { children: React.ReactNode }) => <>{children}</>,
-  root: ({ children }: { children: React.ReactNode }) => (
-    <div className="max-w-prose">{children}</div>
+  pre: ({ children }) => <>{children}</>,
+  root: ({ children }) => (
+    <div className="max-w-prose mx-auto">{children}</div>
   ),
   ol: ({ node, children, ...props }) => {
     return (
